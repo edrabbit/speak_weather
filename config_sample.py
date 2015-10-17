ac8b139d78efedf8e5be7f5575a9969fc02e5639
Ed Hunsinger <edrabbit@edrabbit.com>
2015-10-16 22:43:18 -0700

    Support for saving forecast as txt file

--- config_sample.py
+++ config_sample.py
@@ -2,5 +2,6 @@
 WEATHER_LOCATION="San Francisco, California"
 FORECAST_SAVE_MP3_LOCATION="/path/to/shared/folder/"
 FORECAST_PLAY_MP3_LOCATION="x-file-cifs://network/path/to/shared/folder/"
+FORECAST_SAVE_TXT_LOCATION="/path/to/temp/folder"
 SONOS_IP="192.168.1.20"
 
--- speak_weather.py
+++ speak_weather.py
@@ -3,6 +3,7 @@
 import argparse
 import datetime
 import os
+import time
 
 import gtts
 import soco
@@ -17,6 +18,19 @@ def get_weather():
     weather = client.fetch_weather(woeid)
     return weather
 
+def forecast_to_txt(weather, save_location):
+    for day in range(2):
+        f = weather['forecast'][day]
+        f_date = datetime.datetime.strptime(f['date'], "%d %b %Y")
+        if f_date.date() >= datetime.datetime.today().date():
+            forecast_str = (
+                "The forecast for %s, %s %s "
+                "is %s with a high of %s and a low of %s"
+                % (f_date.strftime("%A"), f_date.strftime("%B"), f_date.day,
+                   f['text'], f['high'], f['low']))
+            fp = open('%sforecast%d.txt' % (save_location, day), "w")
+            fp.write(forecast_str)
+            fp.close()
 
 def forecast_to_mp3(weather, save_location, lang='en'):
     """
@@ -55,17 +69,25 @@ def play_forecast(sonos_ip, play_location):
     sonos = soco.SoCo(sonos_ip)
     # If it's after 6pm local time, play tomorrow's forecast
     day = int(datetime.datetime.now().hour > 18)
+    current_track = sonos.get_current_track_info()
     sonos.play_uri(os.path.join(play_location, ("forecast%d.mp3" % day)))
-
+#    sonos.add_uri_to_queue(os.path.join(play_location, ("forecast%d.mp3" % day)))
+#    sonos.play_from_queue(sonos.queue_size-1)
+    time.sleep(30)
+    sonos.remove_from_queue(sonos.queue_size-1)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('--fetch', action='store_const', const=True)
     parser.add_argument('--speak', action='store_const', const=True)
+    parser.add_argument('--text', action='store_const', const=True)
     return parser.parse_args()
 
 if (__name__ == "__main__"):
     args = parse_args()
+    if args.text:
+        weather = get_weather()
+        forecast_to_txt(weather, config.FORECAST_SAVE_TXT_LOCATION)
     if args.fetch:
         weather = get_weather()
         forecast_to_mp3(weather, config.FORECAST_SAVE_MP3_LOCATION)
